df753415a05b6d8f0dee7e832d4e84f6aba672a5
Ivan Pianetti <ipianetti@cent.gov.ar>
2018-06-18 20:01:00 -0300

    Criteria Builder

---
+++ src/MuchWow.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ivanpianetti
+ * Date: 18/6/18
+ * Time: 19:48
+ */
+
+namespace QueryBuilder\MuchWow;
+
+class MuchWow
+{
+    /**
+     * Transforma los parámetros REST en una lista de criterias necesarias para QueryBuilder.
+     * @param array $query
+     * @return array
+     */
+    public static function obtenerCriterias(array $query)
+    {
+        $criterias = [];
+        foreach ($query as $param => $value){
+            if (is_array ($value)){
+                foreach ($value as $criteria => $valor){
+                    array_push ($criterias, [$param, MuchWow::mapCriteria ($criteria), $valor]);
+                }
+            }else{
+                if ($value === true || strtolower ($value) === 'true'){
+                    $value = '1';
+                }elseif ($value === false || strtolower ($value) === 'false'){
+                    $value = '0';
+                }
+                array_push ($criterias, [$param, MuchWow::mapCriteria ('eq'), $value]);
+            }
+        }
+        return $criterias;
+    }
+
+    /**
+     * Traduce las criterias REST en criterias de QueryBuilder
+     * @param $criteria
+     * @return string
+     */
+    private static function mapCriteria($criteria)
+    {
+        $resp = '=';
+        switch(strtolower ($criteria)){
+            case 'ge':
+                $resp = '>=';
+                break;
+            case 'gt':
+                $resp = '>';
+                break;
+            case 'le':
+                $resp = '<=';
+                break;
+            case 'lt':
+                $resp = '<';
+                break;
+            case 'ne':
+                $resp = '!=';
+                break;
+            case 'like':
+            case 'ilike':
+                $resp = 'LIKE';
+                break;
+            default;
+                break;
+        }
+
+        return $resp;
+    }
+}
